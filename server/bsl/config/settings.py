17289617825718a2fc83d8f046b0a4e7c09887d7
DhirajBhakta <dhirajbhakta110@gmail.com>
2017-12-30 02:05:12 +0530

    ngx-admin theme cloned

--- server/bsl/config/settings.py
+++ server/bsl/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'bsl.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'bsl.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
